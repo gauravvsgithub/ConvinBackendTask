ddf49d665bbb7b9430dda8b130a68669a425bc33
geekgaurav <18365813-geekgaurav@users.noreply.replit.com>
2023-04-22 11:39:51 +0000

    redirect to GoogleCalendarRedirectView from GoogleCalendarInitView

--- api/views.py
+++ api/views.py
@@ -1,11 +1,14 @@
+from django.http import HttpResponseRedirect
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import redirect
 
+from google_auth_oauthlib.flow import InstalledAppFlow
+
 
 @api_view(['GET'])
 def getData(request):
-    item = {'message': 'Head to /rest/v1/calendar/init/ end point'}
+    # redirect to /rest/v1/calendar/init
     return redirect('/rest/v1/calendar/init/')
 
 
@@ -17,5 +20,16 @@ def GoogleCalendarRedirectView(request):
 
 @api_view(['GET'])
 def GoogleCalendarInitView(request):
-    item = {'message': 'Calendar Init View'}
-    return Response(item)
+    # Set up the OAuth flow
+    flow = InstalledAppFlow.from_client_secrets_file(
+        'client_secret.json',
+        scopes=['https://www.googleapis.com/auth/calendar'],
+        redirect_uri=
+        'https://convinbackendtask.geekgaurav.repl.co/rest/v1/calendar/redirect/'
+    )
+
+    # Generate the authorization URL and redirect the user to the consent screen
+    auth_url, state = flow.authorization_url(prompt='consent')
+    request.session['state'] = state
+
+    return HttpResponseRedirect(auth_url)
